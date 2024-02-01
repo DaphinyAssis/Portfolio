05cc3498fd872eec2a9b713303203c4c5a413557
Daphiny Assis <assis.daphiny19@gmail.com>
2024-02-01 16:46:51 -0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,29 @@
+var swiper = new Swiper(".slide-container", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    centerSlide: "true",
+    grabCursor:"true",
+    fade:"true",
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicsBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0:{
+            slidesPerView: 1,
+        },
+        520:{
+            slidesPerView:2,
+        },
+        912: {
+            slidesPerView: 3,
+        },
+    },
+  });
